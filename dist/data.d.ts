353b3fa569efb8c10a8df3457291a2e7618fdce5
Hasanur Kazi <96307573+Hasanur121@users.noreply.github.com>
2023-08-23 22:09:27 +0530

    Create data.d.ts

---
+++ dist/data.d.ts
@@ -0,0 +1,4 @@
+import * as types from './types';
+export declare const getPokemon: () => types.PokemonMap;
+export declare const getTypes: () => types.TypeMap;
+export declare const getGenerations: () => types.GenerationMap;
